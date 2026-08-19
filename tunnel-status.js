6a4c3d80481fb86623db50933c93c2f6ae750b99
ytdownloadweb <ytandinstadownload@gmail.com>
2026-08-19 17:26:00 +0530

    chore: update tunnel-status.js [skip ci]

--- tunnel-status.js
+++ tunnel-status.js
@@ -1,1 +1,1 @@
-window.tunnelStatus = {"status": "online", "url": "https://land-progressive-rational-trek.trycloudflare.com", "updated": "2026-08-19T11:10:15Z"};
+window.tunnelStatus = {"status": "offline", "url": "", "updated": "2026-08-19T11:56:00Z"};
