9c44d3a0e4c6faa5ce078ef39b2904bb8da9e8e2
ytdownloadweb <ytandinstadownload@gmail.com>
2026-08-20 08:43:38 +0530

    chore: update tunnel-status.js [skip ci]

--- tunnel-status.js
+++ tunnel-status.js
@@ -1,1 +1,1 @@
-window.tunnelStatus = {"status": "online", "url": "https://draw-cruises-shade-archived.trycloudflare.com", "updated": "2026-08-19T22:58:04Z"};
+window.tunnelStatus = {"status": "offline", "url": "", "updated": "2026-08-20T03:13:38Z"};
