4c3927b4256908b85c263d9bc4779b0ee8e7ed32
ytdownloadweb <ytandinstadownload@gmail.com>
2026-08-22 04:28:14 +0530

    chore: update tunnel-status.js [skip ci]

--- tunnel-status.js
+++ tunnel-status.js
@@ -1,1 +1,1 @@
-window.tunnelStatus = {"status": "offline", "url": "", "updated": "2026-08-21T22:56:01Z"};
+window.tunnelStatus = {"status": "online", "url": "https://acne-pros-jimmy-delegation.trycloudflare.com", "updated": "2026-08-21T22:58:13Z"};
