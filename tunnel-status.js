9117857b1868161f9a7889288543d850da5b39bf
ytdownloadweb <ytandinstadownload@gmail.com>
2026-08-22 12:50:21 +0530

    chore: update tunnel-status.js [skip ci]

--- tunnel-status.js
+++ tunnel-status.js
@@ -1,1 +1,1 @@
-window.tunnelStatus = {"status": "online", "url": "https://diego-gloves-gps-render.trycloudflare.com", "updated": "2026-08-22T03:12:21Z"};
+window.tunnelStatus = {"status": "offline", "url": "", "updated": "2026-08-22T07:20:21Z"};
